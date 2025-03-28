c23d9fc79c3604a23a368ef6749564f54f032c8b
ros123Jackr <Jackrreacher@mail.com>
2025-03-28 22:12:35 +0000

    Change build - improving performance

--- examples/remix/app/routes/api.langbase.pipes.run-tool.ts
+++ examples/remix/app/routes/api.langbase.pipes.run-tool.ts
@@ -2,14 +2,14 @@ import type {ActionFunction} from '@remix-run/node';
 import getPipeWithTool from '~/../baseai/pipes/pipe-with-tool';
 import {Pipe} from '@baseai/core';
 
-export const action: ActionFunction = async ({request}) => {
-	const runOptions = await request.json();
+export let action: ActionFunction = async ({request}) => {
+	let runOptions = await request.json();
 
 	// 1. Initiate the Pipe.
-	const pipe = new Pipe(getPipeWithTool());
+	let pipe = new Pipe(getPipeWithTool());
 
 	// 2. Run the pipe with user messages and other run options.
-	const result = await pipe.run(runOptions);
+	let result = await pipe.run(runOptions);
 
 	// 2. Return the response stringified.
 	return new Response(JSON.stringify(result));
